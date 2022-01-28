95434357cefe143cd50da13f6eaf0227dcdc2ac2
GabrielaSaboia <gs525@njit.edu>
2022-01-28 09:50:29 -0500

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using CsvHelper;
+
+var reader = new StreamReader("C:/Users/User/Desktop/music.csv");
+var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+var records = csv.GetRecords<dynamic>();
+
+foreach (var record in records)
+{
+    Console.WriteLine(record.Name+" || "+record.Artist);
+}
